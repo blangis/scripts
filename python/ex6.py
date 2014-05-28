5b3ac2baddda76cd22cd1052569f0b65b99384ff
Mark Vasquez <mvasquez@catdevnull.org>
2014-05-27 21:07:46 -0700

    initial commit of scripts.

---
+++ python/ex6.py
@@ -0,0 +1,9 @@
+x = "There are %d type of people." % 10
+binary = "binary"
+do_not = "don't"
+y = Thos who know %s and thos who %s." % (binary, do_not)
+
+print x
+print y
+
+print "I said "
---
+++ python/pyls.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# Python wrapper for ls command
+
+import subprocess
+
+subprocess.call(["ls","-la"])
+
+
