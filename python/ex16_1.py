d7a0e02299bcb08fca8292d433a01b1cc51342ca
Mark Vasquez <mvasquez@catdevnull.org>
2014-06-28 21:15:27 -0700

    add chapter 16 exercises.

--- python/ex16_1.py
+++ python/ex16_1.py
@@ -22,12 +22,12 @@ line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file. "
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write('%s\n%s\n%s\n' % (line1,line2,line3))
+#target.write("\n")
+#target.write(line2)
+#target.write("\n")
+#target.write(line3)
+#target.write("\n")
 
 print "And finally, we close it."
 target.close()
