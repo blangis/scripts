d6418f38cccf02a2f43326f9045df5264b8e49da
Mark Vasquez <mvasquez@catdevnull.org>
2015-10-24 09:57:37 -0700

    new sysinfo.py

--- python/network_programing/sys_info.py
+++ python/network_programing/sys_info.py
@@ -6,6 +6,7 @@ import subprocess
 #Command 1
 uname = "uname"
 uname_arg = "-a"
+print "\n"
 print "Gathering system information with %s command:\n" % uname
 subprocess.call([uname, uname_arg])
 subprocess.call([uname, uname_arg])
@@ -13,6 +14,14 @@ subprocess.call([uname, uname_arg])
 #commnad 2
 diskspace = "df"
 diskspace_arg = "-h"
+print "\n"
 print "Gathering diskspace information %s commnad:\n" % diskspace
 subprocess.call([diskspace, diskspace_arg])
+
+#command 3
+ip_address = "ifconfig"
+ip_address_arg = "-a"
+print "\n"
+print "Gathering ip information %s command:\n" % ip_address
+subprocess.call([ip_address, ip_address_arg])
 
