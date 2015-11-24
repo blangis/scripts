f76c82e528a0378f4a57a09741152c101a02e836
Mark Vasquez <mvasquez@catdevnull.org>
2015-11-23 22:06:37 -0800

    catching up on a few things.

---
+++ python/ex5.py
@@ -0,0 +1,18 @@
+name = 'Mark R. Vasquez'
+age = 46
+height = 72 # inches
+weight = 200
+eyes = 'brown'
+teeth = 'brownish-yellow'
+hair = 'Dark Brown'
+omg = "Too fucking fat!!!1!"
+
+print "Let's talk about %s." % name
+print "He is %d inches tall." % height
+print "He is %d pounds heavy." % weight
+print "He is way too fat.%s" % omg
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "his teeth are usually %s and his breath smells like shit." % teeth
+
+print "If I add %d, %d, and %d I get %d" % (age, height, weight, age + height + weight)
+
---
+++ python/old/ex1.py
@@ -0,0 +1,7 @@
+print "Hello World!"
+print "Hello again."
+print "I like typing alot."
+print "This is fucking fun."
+print "PRINTING, PRINTIING."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
