2134c4d2cce84d58ee5d2194d0f9a60d3be26698
Mark Vasquez <mvasquez@catdevnull.org>
2015-11-27 08:45:15 -0800

    adding new py exercises.

--- python/ex5.py
+++ python/ex5.py
@@ -1,15 +1,15 @@
 name = 'Mark R. Vasquez'
 age = 46
-height = 72 # inches
-weight = 200
+height = 72 * 2.540 # inches multiplied by centimeters.
+weight = 200 * 0.454 # weight in pounds converted to kilos. Which is gay.
 eyes = 'brown'
 teeth = 'brownish-yellow'
 hair = 'Dark Brown'
 omg = "Too fucking fat!!!1!"
 
 print "Let's talk about %s." % name
-print "He is %d inches tall." % height
-print "He is %d pounds heavy." % weight
+print "He is %d centimeters tall." % height
+print "He is %d kilos heavy." % weight
 print "He is way too fat.%s" % omg
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "his teeth are usually %s and his breath smells like shit." % teeth
